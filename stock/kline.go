d68c59599b33245f33303a1e61668139d57d2d4d
wangfeng <wangfengxy@sina.cn>
2023-04-09 18:22:26 +0800

    删除废弃的代码

--- stock/kline.go
+++ stock/kline.go
@@ -15,7 +15,6 @@ func KLine(code string, argv ...bool) pandas.DataFrame {
 		weekday = stat.AnyToBool(argv[0])
 	}
 	df9 := tdx.GetCacheKLine(code, true)
-	//df9 = attachVolume(df9, code)
 	if !weekday {
 		return df9
 	}

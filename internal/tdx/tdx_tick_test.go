bcf15124ede86b90478af2eccc6dc72c35ef3f98
wangfeng <wangfengxy@sina.cn>
2023-04-13 18:20:32 +0800

    调整测试代码

--- internal/tdx/tdx_tick_test.go
+++ internal/tdx/tdx_tick_test.go
@@ -5,7 +5,12 @@ import (
 	"testing"
 )
 
-func TestGetTickAll(t *testing.T) {
+func TestGetTickData(t *testing.T) {
 	df := GetTickData("sh600600", "2023-04-04")
 	fmt.Println(df)
+}
+
+func TestGetTickAll(t *testing.T) {
+	code := "sz002351"
+	GetTickAll(code)
 }
